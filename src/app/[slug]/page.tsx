028e84718c61ab6c7abd3b9f7bd1cc6444b807f3
ayushmaan-mishra203 <142373193+ayushmaan-mishra203@users.noreply.github.com>
2025-08-01 08:52:34 +0530

    first commit

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const singlepage = () => {
+  return (
+    <div>singlepage</div>
+  )
+}
+
+export default singlepage
---
+++ src/components/NavIcons.tsx
@@ -0,0 +1,11 @@
+"useclient"
+
+import React from 'react'
+
+const NavIcons = () => {
+  return (
+    <div>NavIcons</div>
+  )
+}
+
+export default NavIcons
